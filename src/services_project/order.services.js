e8bfbd211da96528cd30f10483269e569bca9158
Santiago Arboleda Agudelo <106878837+sarboleda72@users.noreply.github.com>
2025-11-06 18:03:16 -0500

    prueba codigo tipos variables

--- src/services_project/order.services.js
+++ src/services_project/order.services.js
@@ -1,1 +1,13 @@
 console.log("este es el servicio de usuarios");
+
+const TEXT = "este es el servicio de ordenes";
+
+const PascalCaseText = TEXT;
+
+const camelCaseText = TEXT;
+
+const snake_case_text = TEXT;
+
+const UPPERCASETEXT = TEXT;
+
+const UPPER_SNAKE_CASE_TEXT = TEXT;
